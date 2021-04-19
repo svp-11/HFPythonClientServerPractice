6e7ecd38ad4dfa98e7ae8a0da2fe9e6f69c41d0a
Valery Smalko <smalko9493@yandex.by>
2021-04-19 16:38:41 +0300

    first add new project

---
+++ vsearch4web.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from vsearch import search4letters
+app = Flask(__name__)
+
+@app.route('/search4', methods = ['POST'])
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = "Here your result!"
+    result = str(search4letters(phrase, letters))
+    return (render_template('results.html', the_title = title, the_results = result, the_letters = letters, the_phrase = phrase,))
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title = "Welcome to search4letters on the web!")
+
+if __name__ == 'main':
+    app.run(debug=True)
+
+
